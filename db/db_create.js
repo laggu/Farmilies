6e3dfa6e1268aea0149a74962e1412d04851f36f
laggu <kwla103@naver.com>
2018-06-20 11:26:50 +0900

    USERS table 수정

--- db/db_create.js
+++ db/db_create.js
@@ -4,11 +4,13 @@ var user = function (){
     conn = db_conn.db_conn();
     console.log("Success");
     var sql = "CREATE TABLE USERS(" +
+        "id mediumint not null auto_increment," +
         "email varchar(50) not null," +
         "pw char(64) not null," +
         "name varchar(10) not null," +
         "photo varchar(30)," +
-        "primary key(email))";
+        "primary key(id)," +
+        "unique index (email))";
     var result = conn.query(sql, function (err, result){
         if (err){
             throw err;
@@ -18,6 +20,28 @@ var user = function (){
 
     conn.end();
 };
+
+var works = function (){
+    conn = db_conn.db_conn();
+    console.log("Success");
+    var sql = "CREATE TABLE WORKS(" +
+        "id mediumint not null auto_increment," +
+        "email varchar(50) not null," +
+        "pw char(64) not null," +
+        "name varchar(10) not null," +
+        "photo varchar(30)," +
+        "primary key(id))";
+    var result = conn.query(sql, function (err, result){
+        if (err){
+            throw err;
+        }
+        console.log("USER TABLE CREATED");
+    });
+
+    conn.end();
+};
+
+
 
 exports.user = user;
 

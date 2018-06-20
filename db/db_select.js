37ed1110467e4da43f7238a86507c50bf9916862
laggu <kwla103@naver.com>
2018-06-20 10:26:56 +0900

    user insert and select 완료

--- db/db_select.js
+++ db/db_select.js
@@ -1,25 +1,23 @@
 var db_conn = require('./db_conn');
 
-var user = function (user_email){
+var user = function (user_email, callback){
     conn = db_conn.db_conn();
     var sql = "SELECT * FROM users WHERE EMAIL = '" + user_email + "'";
-
-    var result = conn.query(sql, function (err, result, fields){
-        if (err){
-            //console.log(123);
-            throw err;
-        }
-        //console.log(23);
-        console.log("SELECT USER SUCCESS");
-        //console.log(result);
-        var person = result[0];
-        return person;
-    });
-    console.log(result);
-    return result;
+    conn.query(sql, callback );
+    conn.end();
 };
 
-console.log('asd');
-a = user('abc@naver.com');
+
+/*
+var p = function (err, result, fields){
+    if (err){
+        throw err;
+    }
+    console.log("SELECT USER SUCCESS");
+    person = JSON.stringify(result[0]);
+    console.log(person);
+};
+
+a = user('abc@naver.com', p);
 console.log(a);
-console.log(a.email);
+*/

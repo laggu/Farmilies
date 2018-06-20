f0f4c8795d9c6b5bb93cb77c1ed3e55b164d65c9
laggu <kwla103@naver.com>
2018-06-20 15:17:52 +0900

    charset 변경

--- db/db_create.js
+++ db/db_create.js
@@ -10,8 +10,9 @@ var users = function (){
         "name varchar(10) not null," +
         "photo varchar(30)," +
         "primary key(id)," +
-        "unique index (email))";
-    var result = conn.query(sql, function (err, result){
+        "unique index (email)" +
+        ") engine = InnoDB default charset=utf8";
+    conn.query(sql, function (err, result){
         if (err){
             throw err;
         }
@@ -30,8 +31,8 @@ var locations = function (){
         "longitude double not null," +
         "address_name varchar(100) not null," +
         "primary key(id)" +
-        ")";
-    var result = conn.query(sql, function (err, result){
+        ") engine = InnoDB default charset=utf8";
+    conn.query(sql, function (err, result){
         if (err){
             throw err;
         }
@@ -49,12 +50,13 @@ var works = function (){
         "farmer_id int not null," +
         "title char(100) not null," +
         "description varchar(500) not null," +
+        "reward int not null," +
         "location_id int not null," +
         "primary key(id)," +
         "foreign key (farmer_id) references USERS(id) on delete cascade," +
         "foreign key (location_id) references LOCATIONS(id) on delete cascade" +
-        ")";
-    var result = conn.query(sql, function (err, result){
+        ") engine = InnoDB default charset=utf8";
+    conn.query(sql, function (err, result){
         if (err){
             throw err;
         }
@@ -64,12 +66,36 @@ var works = function (){
     conn.end();
 };
 
+var contracts = function(){
+    conn = db_conn.db_conn();
+    console.log("Success");
+    var sql = "create table if not exists CONTRACTS(" +
+        "id int not null auto_increment," +
+        "citizen_id int not null," +
+        "work_id int not null," +
+        "date datetime not null," +
+        "primary key(id)," +
+        "foreign key (citizen_id) references USERS(id) on delete cascade," +
+        "foreign key (work_id) references WORKS(id) on delete cascade" +
+        ") engine = InnoDB default charset=utf8";
+    conn.query(sql, function (err, result){
+        if (err){
+            throw err;
+        }
+        console.log("CONTRACTS TABLE CREATED");
+    });
 
+    conn.end();
+};
 
 exports.users = users;
 exports.locations = locations;
 exports.works = works;
+exports.contracts = contracts;
 
+/*
 users();
 locations();
 works();
+contracts();
+*/
